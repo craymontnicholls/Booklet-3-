11c61a168205d9adb4858d8334d97fdf23324bc5
craymontnicholls <73825801+craymontnicholls@users.noreply.github.com>
2020-11-23 07:24:17 +0000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#takes parmeter age then finds how old the dog is in dog years
-def dog(age):
-  if age <= 2:
-    return (age * 12)
-  elif age > 2:
-    #not sure if it is (age - 2)*6 or just age * 6
-    return (((age -2)  * 6) + 22)
-
-print(dog(2.5))
